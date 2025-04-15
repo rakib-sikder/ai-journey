351bfad59cf8e715fd894764b881a639a6dff93e
rakib-sikder <itsmebroiam@gmail.com>
2025-04-15 14:16:13 +0600

    variables and types

---
+++ Variables and types/main.py
@@ -0,0 +1,9 @@
+name = "rakib" 
+age = 22
+height = 5.7 
+is_Developer = True
+
+print(type(name),type(age),type(height),type(is_Developer)) 
+
+
+print("my name is "+ name+" and my age is "+ str(age) + " and my height is "+ str(height) + " and I am a developer: "+ str(is_Developer))
