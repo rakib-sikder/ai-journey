1f048c0c56bb142ec2325566f61538164d34c348
rakib-sikder <itsmebroiam@gmail.com>
2025-04-15 14:35:54 +0600

    string manipulation

---
+++ in build Func/main.py
@@ -0,0 +1,12 @@
+name = "rakib"
+
+print(len(name)) # length of string
+print(name.find("a")) # find the index of a character in string
+print(name.count("r")) # count the number of times a character appears in string
+print(name.upper()) # convert string to upper case
+print(name.lower()) # convert string to lower case
+print(name.replace("a", "A")) # replace a character in string
+print(name.isalpha()) # check if string is alphabetic
+print(name.isdigit()) # check if string is digit
+print(name.capitalize()) # capitalize the first letter of string
+print(name *10) # repeat the string 10 times
