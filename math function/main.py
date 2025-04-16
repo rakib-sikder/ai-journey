d630ce12d5d63072482b4b1d1d0bfa17f9074441
rakib-sikder <itsmebroiam@gmail.com>
2025-04-16 12:07:16 +0600

    math functions

---
+++ math function/main.py
@@ -0,0 +1,16 @@
+import math
+pi = 3.1416
+radius = 5
+a=1
+b=2
+c=3
+
+print(round(pi))
+print(math.ceil(pi))
+print(math.floor(pi))
+print(abs(-pi))
+print(pow(c ,b))
+print(math.sqrt(10))
+print(math.factorial(5))
+max=max(a,b,c)
+min = min(a,b,c)
