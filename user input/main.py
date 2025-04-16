c5288eeaf6fd62d133358ea49d96cdc0c3f9c7ab
rakib-sikder <itsmebroiam@gmail.com>
2025-04-16 11:45:48 +0600

    user input

---
+++ user input/main.py
@@ -0,0 +1,8 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+height = input("Enter your height: ")
+
+age = int(age)+1
+height = float(height)+1.0
+
+print("my name is "+name+" i am "+str(age)+"i am "+str(height)+" feet tall")
